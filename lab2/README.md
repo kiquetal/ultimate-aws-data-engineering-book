cc8875ae0d8f9b8ec6f91bc55290525d47ef27bb
kiquetal <kiquetal@gmail.com>
2025-05-23 12:58:01 -0300

    add environment context support for deploying stacks

--- lab2/README.md
+++ lab2/README.md
@@ -12,3 +12,23 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+## Deploying with environment context
+
+You can specify which stack to deploy using the CDK context flag:
+
+- Deploy the default stack:
+  ```sh
+  npx cdk deploy
+  ```
+- Deploy the prod stack:
+  ```sh
+  npx cdk deploy -c env=prod
+  ```
+
+Alternatively, you can use the environment variable:
+
+  ```sh
+  DEPLOY_ENV=prod npx cdk deploy
+  ```
+
--- lab2/bin/lab2.ts
+++ lab2/bin/lab2.ts
@@ -3,18 +3,25 @@ import * as cdk from 'aws-cdk-lib';
 import { Lab2Stack } from '../lib/lab2-stack';
 
 const app = new cdk.App();
-new Lab2Stack(app, 'Lab2Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Determine environment from context or environment variable
+const deployEnv = app.node.tryGetContext('env') || process.env.DEPLOY_ENV || 'default';
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+if (deployEnv === 'prod') {
+  new Lab2Stack(app, 'Lab2ProdStack', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+    /* Add any prod-specific props here */
+  });
+} else {
+  new Lab2Stack(app, 'Lab2Stack', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+    /* Add any default-specific props here */
+  });
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
